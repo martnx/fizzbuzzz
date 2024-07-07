d83358bc39d9f986c1f0729ca4ed438a697f2ff8
martnx <144177177+martnx@users.noreply.github.com>
2024-07-07 23:28:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//Fizz buzz practice
+
+const input = parseInt(prompt("Enter to FizzBuzz: "));
+
+
+    for(let i = 1; i <= input; i++){
+        
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("Fizz Buzz");
+        }
+        else if(i % 3 === 0){
+            console.log("Fizz")
+        }
+        else if(i % 5 === 0){
+            console.log("Buzz")
+        }
+
+        else
+        console.log(i);
+    }
+
